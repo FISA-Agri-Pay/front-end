778ae877305051501f56d38121f1230225e008f8
류승환 <peter1256@skuniv.ac.kr>
2026-06-09 16:10:14 +0900

    fix: prefix 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/user/' : '/',
   plugins: [
     react(),
     tailwindcss(),
@@ -27,4 +28,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
